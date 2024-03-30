d791a9c7ebd427e99f2e7c2b3c96d577962bd927
LadyRin <alexandre.letellier09@gmail.com>
2024-03-30 16:14:10 +0100

    dist

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ConsoleView from '../views/ConsoleView.vue'
 import DesktopView from '@/views/DesktopEnvironmentView.vue'
-import AboutPage from '@/components/pages/AboutPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
