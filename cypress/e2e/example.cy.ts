bb3accda39a0cbcce2b49b52234836acf98d9549
LadyRin <alexandre.letellier09@gmail.com>
2024-06-01 21:15:07 +0200

    dist

--- cypress/e2e/example.cy.ts
+++
@@ -1,7 +0,0 @@
-// https://on.cypress.io/api
-
-describe('smoke test', () => {
-  it('visits the app root url', () => {
-    cy.visit('/')
-  })
-})
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-npm run build
-cp dist/index.html dist/404.html
-git add .
-git commit -m "dist"
-git subtree push --prefix dist origin gh-pages
